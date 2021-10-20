1aa18599ca3dde62cc2e239903961a8a0b743ecb
wahyubagus <wahyu.sulaksono@ist.id>
2021-10-20 21:33:42 +0700

    setup : base design

---
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.noteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_ADD_NOTE = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageAddNoteMain = findViewById(R.id.imageAddNoteMain);
+        imageAddNoteMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(
+                        new Intent(getApplicationContext(), CreateNoteActivity.class), REQUEST_CODE_ADD_NOTE
+                );
+            }
+        });
+    }
+}
